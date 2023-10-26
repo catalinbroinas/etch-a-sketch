41d9d6c1a9a4623d6d1a7634a65ea080f1752d38
catalinbroinas <catalinbroinas@yahoo.com>
2023-10-26 17:40:31 +0300

    Add button for clear grid clearGrid(): remove all rows and cols from grid

--- js/style.js
+++ js/style.js
@@ -1,16 +1,16 @@
-function createGrid() 
+function createGrid(numRows, numCols) 
 {
     const grid = document.querySelector('.grid');
     const rows = [];
     const cols = [];
     
-    for (let i = 0; i < 16; i++)
+    for (let i = 0; i < numRows; i++)
     {
         rows[i] = document.createElement('div');
         rows[i].classList.add('row');
         grid.appendChild(rows[i]);
 
-        for (let j = 0; j < 16; j++)
+        for (let j = 0; j < numCols; j++)
         {
             cols[j] = document.createElement('div');
             cols[j].classList.add('col');
@@ -19,9 +19,9 @@ function createGrid()
     }
 }
 
-function penEffect()
+function penEffect(numRows, numCols)
 {
-    createGrid();
+    createGrid(numRows, numCols);
 
     const cells = document.querySelectorAll('.col');
     for(let cell of cells)
@@ -31,4 +31,24 @@ function penEffect()
         });
     }
 }
-window.addEventListener('load', penEffect);
+
+function clearGrid(numRows, numCols)
+{
+    createGrid(numRows, numCols);
+
+    const rows = document.querySelectorAll('.row');
+    const grid = document.querySelector('.grid');
+
+    for (let row of rows)
+    {
+        grid.removeChild(row);
+    }
+}
+
+window.addEventListener('load', (numRows, numCols) => {
+    penEffect(16, 16);
+});
+const changeGrid = document.querySelector('#change-grid');
+changeGrid.addEventListener('click', (numRows, numCols) => {
+    clearGrid(numRows, numCols);
+});
